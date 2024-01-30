80f58213f7cca28064881e25baae431874450a0d
v1dya-sagar <vidyasagar08123@gmail.com>
2024-01-30 17:53:42 +0530

    completed Day 14 code

--- HundreadDays/Day_14_removeDuplicates.java
+++ HundreadDays/Day_14_removeDuplicates.java
@@ -1,4 +1,17 @@
 package HundreadDays;
 
 public class Day_14_removeDuplicates {
+    public static void main(String[] args) {
+        int[] a = {1,2,2,7};
+        int j = 0;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] > a[j]){
+                j++;
+                a[j] = a[i];
+            }
+        }
+        System.out.println(
+                j+1
+        );
+    }
 }
