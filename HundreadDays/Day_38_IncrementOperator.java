15e99ccb8da98fb81893d512d71439c6267cbcc6
v1dya-sagar <vidyasagar08123@gmail.com>
2024-03-11 21:22:23 +0530

    Completed day 38 code

---
+++ HundreadDays/Day_38_IncrementOperator.java
@@ -0,0 +1,10 @@
+package HundreadDays;
+
+public class Day_38_IncrementOperator {
+    public static void main(String[] args) {
+
+//        int b = ++23; this is not possible as ++ and -- can only be used with variables.
+//        int b = ++(++b); this is not possible as ++ and -- can only be used with variables.
+
+    }
+}
