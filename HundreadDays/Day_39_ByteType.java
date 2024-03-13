d0e0f1574ff92c8fe885e8021da0a81db9573c8b
v1dya-sagar <vidyasagar08123@gmail.com>
2024-03-13 23:25:52 +0530

    Changes

--- HundreadDays/Day_39_ByteType.java
+++ HundreadDays/Day_39_ByteType.java
@@ -3,7 +3,11 @@ package HundreadDays;
 public class Day_39_ByteType {
     public static void main(String[] args) {
         byte a = 1, b = 2;
-        // byte c = byte(a+b)
+        System.out.println(a + " " + b);
+        // byte c = byte (a+b)
 
     }
 }
+
+
+
