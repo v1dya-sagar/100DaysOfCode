d049f1202c698625acab0fbe7c86bbd05532266c
v1dya-sagar <vidyasagar08123@gmail.com>
2024-03-18 23:23:53 +0530

    Changes

--- HundreadDays/Day_3_leapYearOrNot.java
+++ HundreadDays/Day_3_leapYearOrNot.java
@@ -12,7 +12,7 @@ public class Day_3_leapYearOrNot {
     }
 
     public static boolean isLeapYear(int year) {
-        return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+        return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0) );
     }
 }
 
